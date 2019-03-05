4f19cc254989e1fd1374b9d8db7b0f75e29ff747
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-04 23:32:40 -0800

    Installed express and added basic middleware

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next();
+    // ...
+});
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    res.send('<h1>Hello From Express</h1>');
+    // ...
+});
 
-server.listen(3000);
+app.listen(3000);
