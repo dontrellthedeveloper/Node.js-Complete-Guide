a54fcc8ac2fedd8f8c5cc554229af63c6fe2b41e
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-08 16:28:34 -0800

    adding layout to handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
-
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add product', path: '/admin/add-product'});
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 });
 
 // /admin/add-product => POST
