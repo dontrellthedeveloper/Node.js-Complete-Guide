0c79a8e4e31a86a48aca1766eb3ffe2c7126a8aa
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-10 12:54:45 -0700

    pre-populating the edit product page with data & linking to the edit page

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 const express = require('express');
 
-const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
-
-
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
@@ -18,5 +16,7 @@ router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product');
 
 module.exports = router;
