72ec667f85cc2939d5743efc0bef398a33df07d4
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-17 07:43:22 -0700

    fetching all products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,18 +1,19 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then(products => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    Product.fetchAll()
+        .then(products => {
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'All Products',
+                path: '/products'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
+
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -37,7 +38,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/index', {
         prods: products,
