b9f2efcafbb98507c9917ccda791a345a7a8477a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-17 08:45:16 -0700

    Another way to implement ObjectId

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,5 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
 
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -63,7 +61,7 @@ exports.postEditProduct = (req, res, next) => {
         updatedPrice,
         updatedDesc,
         updatedImageUrl,
-        new ObjectId(prodId)
+        prodId
     );
     product
         .save()
