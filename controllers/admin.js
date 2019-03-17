cb0525b038a08289513442952092d4f27ef1e578
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-17 13:43:21 -0700

    creating new users and storing users in the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,11 +10,18 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(
+        title,
+        price,
+        description,
+        imageUrl,
+        null,
+        req.user._id
+    );
     product
         .save()
         .then(result => {
