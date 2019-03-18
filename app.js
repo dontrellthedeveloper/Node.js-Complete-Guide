621229fa1755611588d1802a7892c39abfff8d87
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-17 21:37:31 -0700

    using mongodb to store sessions

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+MONGODB_URI = 'mongodb+srv://Dontrell:J1wmIx64mBI3vDA7@nodejs-nindc.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +27,12 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({ secret: 'my secret', resave: false, saveUninitialized: false })
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
 );
 app.use((req, res, next) => {
     User.findById('5c8ef7dd786bd84fdf389920')
@@ -39,7 +51,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        'mongodb+srv://Dontrell:J1wmIx64mBI3vDA7@nodejs-nindc.mongodb.net/shop?retryWrites=true',
+        MONGODB_URI,
         {useNewUrlParser: true}
     )
     .then(result => {
