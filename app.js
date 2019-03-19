daf72c4fd1d793da69bc5215436cf2f7ad1a9a49
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-18 22:08:11 -0700

    Added starter changes for authentication and implemented a workflow

--- app.js
+++ app.js
@@ -57,21 +57,8 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Dontrell',
-                    email: 'dontrellknight@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(3000);
     })
     .catch(err => {
         console.log(err);
     });
-
