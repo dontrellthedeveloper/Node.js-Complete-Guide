029ddafe5eaa92af73d190a5729ddf8a56f34ac9
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-23 19:54:17 -0700

    added sendgrid to send an email confirmation once a user signs up.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: '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'
+    }
+}));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -82,6 +90,15 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then(result => {
                     res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'shop@node-complete.com',
+                        subject: 'Signup succeeded!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    });
+
+                }).catch(err => {
+                    console.log(err);
                 });
         })
         .catch(err => {
