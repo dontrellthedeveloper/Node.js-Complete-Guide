8a2cb55383834049dc5131842917fe643ab80b7c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-23 19:34:26 -0700

    providing user feedback when they log in wrong. Also provided a styled error messages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -38,6 +39,7 @@ app.use(
     })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -56,7 +58,6 @@ app.use((req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 });
-
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
