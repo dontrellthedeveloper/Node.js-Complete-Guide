4b3d779f30b936d1c7e30fe0679d8acfc00c8963
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-03-24 18:39:01 -0700

    Add ability to store files in the database. Also serve images statically. Add 500 error if something happens.

--- routes/admin.js
+++ routes/admin.js
@@ -22,7 +22,6 @@ router.post(
             .isString()
             .isLength({ min: 3 })
             .trim(),
-        body('imageUrl').isURL(),
         body('price').isFloat(),
         body('description')
             .isLength({ min: 5, max: 400 })
@@ -53,4 +52,5 @@ router.post(
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
+
 
